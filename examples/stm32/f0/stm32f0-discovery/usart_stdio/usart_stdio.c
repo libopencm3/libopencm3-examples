4ae45b913d2f15883815f6c93c0d20d968c6331e
BuFran <BuFran@seznam.cz>
2013-07-10 22:37:30 +0200

    [EXAMPLE:F0] Add usart printf example using newlib-nano

---
+++ examples/stm32/f0/stm32f0-discovery/usart_stdio/usart_stdio.c
@@ -0,0 +1,119 @@
+/*
+ * This file is part of the libopencm3 project.
+ *
+ * Copyright (C) 2009 Uwe Hermann <uwe@hermann-uwe.de>
+ * Copyright (C) 2011 Stephen Caudle <scaudle@doceme.com>
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/usart.h>
+#include <stdio.h>
+#include <errno.h>
+#include <stddef.h>
+#include <sys/types.h>
+
+static ssize_t _iord(void *_cookie, char *_buf,size_t _n);
+static ssize_t _iowr(void *_cookie,const char *_buf, size_t _n);
+
+static ssize_t _iord(void *_cookie, char *_buf,size_t _n)
+{
+	/* dont support reading now */
+	(void)_cookie;
+	(void)_buf;
+	(void)_n;
+	return 0;
+}
+
+static ssize_t _iowr(void *_cookie,const char *_buf, size_t _n)
+{
+	uint32_t dev = (uint32_t)_cookie;
+
+	int written = 0;
+	while (_n-- > 0) {
+		usart_send_blocking(dev, *_buf++);
+		written++;
+	};
+	return written;
+}
+
+
+static FILE* usart_setup(uint32_t dev)
+{
+	/* Setup USART2 parameters. */
+	usart_set_baudrate(dev, 38400);
+	usart_set_databits(dev, 8);
+	usart_set_parity(dev, USART_PARITY_NONE);
+	usart_set_stopbits(dev, USART_CR2_STOP_1_0BIT);
+	usart_set_mode(dev, USART_MODE_TX_RX);
+	usart_set_flow_control(dev, USART_FLOWCONTROL_NONE);
+
+	/* Finally enable the USART. */
+	usart_enable(dev);
+	
+	cookie_io_functions_t stub = { _iord, _iowr, NULL, NULL };
+	FILE *fp = fopencookie((void *)dev,"rw+",stub);
+	/* Do not buffer the serial line */
+	setvbuf(fp, NULL, _IONBF, 0);
+	return fp;
+
+}
+
+static void clock_setup(void)
+{
+	/* Enable GPIOC clock for LED & USARTs. */
+	rcc_periph_clock_enable(RCC_GPIOC);
+	rcc_periph_clock_enable(RCC_GPIOA);
+
+	/* Enable clocks for USART2. */
+	rcc_periph_clock_enable(RCC_USART1);
+}
+
+static void gpio_setup(void)
+{
+	/* Setup GPIO pin GPIO8/9 on GPIO port C for LEDs. */
+	gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO8 | GPIO9);
+
+	/* Setup GPIO pins for USART2 transmit. */
+	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO9);
+
+	/* Setup USART1 TX pin as alternate function. */
+	gpio_set_af(GPIOA, GPIO_AF1, GPIO9);
+}
+
+int main(void)
+{
+	int i, c = 0;
+	FILE *fp;
+
+	clock_setup();
+	gpio_setup();
+	fp = usart_setup(USART1);
+
+	/* Blink the LED (PD12) on the board with every transmitted byte. */
+	while (1) {
+		gpio_toggle(GPIOC, GPIO8);	/* LED on/off */
+		
+		fprintf(fp,"Pass: %d\n",c);
+		
+		c = (c == 200) ? 0 : c + 1;	/* Increment c. */
+		
+		for (i = 0; i < 1000000; i++)	/* Wait a bit. */
+			__asm__("NOP");
+	}
+
+	return 0;
+}
