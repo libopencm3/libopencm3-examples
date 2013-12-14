c5d6480201543c607664df4455e9337db305f0b6
Frantisek Burian <BuFran@seznam.cz>
2013-12-14 19:18:22 +0100

    [EXAMPLE:F0] Add simple ADC example

---
+++ examples/stm32/f0/stm32f0-discovery/adc/adc.c
@@ -0,0 +1,131 @@
+/*
+ * This file is part of the libopencm3 project.
+ *
+ * Copyright (C) 2009 Uwe Hermann <uwe@hermann-uwe.de>
+ * Copyright (C) 2011 Stephen Caudle <scaudle@doceme.com>
+ * Modified by Fernando Cortes <fermando.corcam@gmail.com>
+ * modified by Guillermo Rivera <memogrg@gmail.com>
+ * modified by Frantisek Burian <BuFran@seznam.cz> 
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/adc.h>
+#include <libopencm3/stm32/usart.h>
+#include <libopencm3/stm32/gpio.h>
+
+uint8_t channel_array[] = { ADC_CHANNEL1, ADC_CHANNEL1, ADC_CHANNEL_TEMP};
+
+static void adc_setup(void)
+{
+	rcc_periph_clock_enable(RCC_ADC);
+	rcc_periph_clock_enable(RCC_GPIOA);
+
+	gpio_mode_setup(GPIOA, GPIO_MODE_ANALOG, GPIO_PUPD_NONE, GPIO0);
+	gpio_mode_setup(GPIOA, GPIO_MODE_ANALOG, GPIO_PUPD_NONE, GPIO1);
+
+	adc_power_off(ADC1);
+	adc_set_clk_source(ADC1, ADC_CLKSOURCE_ADC);
+	adc_calibrate_start(ADC1);
+	adc_calibrate_wait_finish(ADC1);
+	adc_set_operation_mode(ADC1, ADC_MODE_SCAN);
+	adc_disable_external_trigger_regular(ADC1);
+	adc_set_right_aligned(ADC1);
+	adc_enable_temperature_sensor();
+	adc_set_sample_time_on_all_channels(ADC1, ADC_SMPTIME_071DOT5);
+	adc_set_regular_sequence(ADC1, 1, channel_array);
+	adc_set_resolution(ADC1, ADC_RESOLUTION_12BIT);
+	adc_disable_analog_watchdog(ADC1);
+	adc_power_on(ADC1);
+
+	/* Wait for ADC starting up. */
+	int i;
+	for (i = 0; i < 800000; i++)    /* Wait a bit. */
+		__asm__("nop");
+
+}
+
+static void usart_setup(void)
+{
+	/* Enable clocks for GPIO port A (for GPIO_USART2_TX) and USART1. */
+	rcc_periph_clock_enable(RCC_USART1);
+	rcc_periph_clock_enable(RCC_GPIOA);
+
+	/* Setup GPIO pin GPIO_USART1_TX/GPIO9 on GPIO port A for transmit. */
+	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO9);
+	gpio_set_af(GPIOA, GPIO_AF1, GPIO9);
+
+	/* Setup UART parameters. */
+	usart_set_baudrate(USART1, 38400);
+	usart_set_databits(USART1, 8);
+	usart_set_stopbits(USART1, USART_CR2_STOP_1_0BIT);
+	usart_set_mode(USART1, USART_MODE_TX);
+	usart_set_parity(USART1, USART_PARITY_NONE);
+	usart_set_flow_control(USART1, USART_FLOWCONTROL_NONE);
+
+	/* Finally enable the USART. */
+	usart_enable(USART1);
+}
+
+static void my_usart_print_int(uint32_t usart, int16_t value)
+{
+	int8_t i;
+	int8_t nr_digits = 0;
+	char buffer[25];
+
+	if (value < 0) {
+		usart_send_blocking(usart, '-');
+		value = value * -1;
+	}
+
+	if (value == 0) {
+		usart_send_blocking(usart, '0');
+	}
+
+	while (value > 0) {
+		buffer[nr_digits++] = "0123456789"[value % 10];
+		value /= 10;
+	}
+
+	for (i = nr_digits-1; i >= 0; i--) {
+		usart_send_blocking(usart, buffer[i]);
+	}
+
+	usart_send_blocking(usart, '\r');
+	usart_send_blocking(usart, '\n');
+}
+
+int main(void)
+{
+	uint16_t temp;
+
+	adc_setup();
+	usart_setup();
+
+	while (1) {
+		adc_start_conversion_regular(ADC1);
+		while (!(adc_eoc(ADC1)));
+
+		temp = adc_read_regular(ADC1);
+		my_usart_print_int(USART1, temp);
+		
+		int i;
+		for (i = 0; i < 800000; i++)    /* Wait a bit. */
+			__asm__("nop");
+	}
+
+	return 0;
+}
+
