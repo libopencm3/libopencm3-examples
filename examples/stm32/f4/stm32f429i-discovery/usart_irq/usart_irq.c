7bf64424ab7336597b54d974b60aa099719633e1
Piotr Esden-Tempski <piotr@esden.net>
2015-01-14 18:14:40 -0800

    [stm32f429i-discovery] Added USART IRQ example.

---
+++ examples/stm32/f4/stm32f429i-discovery/usart_irq/usart_irq.c
@@ -0,0 +1,115 @@
+/*
+ * This file is part of the libopencm3 project.
+ *
+ * Copyright (C) 2009 Uwe Hermann <uwe@hermann-uwe.de>
+ * Copyright (C) 2011 Stephen Caudle <scaudle@doceme.com>
+ * Copyright (C) 2013 Piotr Esden-Tempski <piotr@esden.net>
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/usart.h>
+#include <libopencm3/cm3/nvic.h>
+
+static void clock_setup(void)
+{
+	/* Enable GPIOG clock for LED & USARTs. */
+	rcc_periph_clock_enable(RCC_GPIOG);
+	rcc_periph_clock_enable(RCC_GPIOA);
+
+	/* Enable clocks for USART1. */
+	rcc_periph_clock_enable(RCC_USART1);
+}
+
+static void usart_setup(void)
+{
+	/* Enable the USART1 interrupt. */
+	nvic_enable_irq(NVIC_USART1_IRQ);
+
+	/* Setup GPIO pins for USART1 transmit. */
+	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO9);
+
+	/* Setup GPIO pins for USART1 receive. */
+	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO10);
+	gpio_set_output_options(GPIOA, GPIO_OTYPE_OD, GPIO_OSPEED_25MHZ, GPIO10);
+
+	/* Setup USART1 TX and RX pin as alternate function. */
+	gpio_set_af(GPIOA, GPIO_AF7, GPIO9);
+	gpio_set_af(GPIOA, GPIO_AF7, GPIO10);
+
+	/* Setup USART1 parameters. */
+	usart_set_baudrate(USART1, 38400);
+	usart_set_databits(USART1, 8);
+	usart_set_stopbits(USART1, USART_STOPBITS_1);
+	usart_set_mode(USART1, USART_MODE_TX_RX);
+	usart_set_parity(USART1, USART_PARITY_NONE);
+	usart_set_flow_control(USART1, USART_FLOWCONTROL_NONE);
+
+	/* Enable USART1 Receive interrupt. */
+	usart_enable_rx_interrupt(USART1);
+
+	/* Finally enable the USART. */
+	usart_enable(USART1);
+}
+
+static void gpio_setup(void)
+{
+	/* Setup GPIO pin GPIO13 on GPIO port G for LED. */
+	gpio_mode_setup(GPIOG, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO13);
+}
+
+int main(void)
+{
+	clock_setup();
+	gpio_setup();
+	usart_setup();
+
+	while (1) {
+		__asm__("NOP");
+	}
+
+	return 0;
+}
+
+void usart1_isr(void)
+{
+	static uint8_t data = 'A';
+
+	/* Check if we were called because of RXNE. */
+	if (((USART_CR1(USART1) & USART_CR1_RXNEIE) != 0) &&
+	    ((USART_SR(USART1) & USART_SR_RXNE) != 0)) {
+
+		/* Indicate that we got data. */
+		gpio_toggle(GPIOG, GPIO13);
+
+		/* Retrieve the data from the peripheral. */
+		data = usart_recv(USART1);
+
+		/* Enable transmit interrupt so it sends back the data. */
+		usart_enable_tx_interrupt(USART1);
+	}
+
+	/* Check if we were called because of TXE. */
+	if (((USART_CR1(USART1) & USART_CR1_TXEIE) != 0) &&
+	    ((USART_SR(USART1) & USART_SR_TXE) != 0)) {
+
+		/* Put data into the transmit register. */
+		usart_send(USART1, data);
+
+		/* Disable the TXE interrupt as we don't need it anymore. */
+		usart_disable_tx_interrupt(USART1);
+	}
+}
