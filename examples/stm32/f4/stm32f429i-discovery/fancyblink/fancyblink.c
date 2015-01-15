e8694d561ff2d72457d8ad4fd4eaa002f8adb584
Piotr Esden-Tempski <piotr@esden.net>
2015-01-14 16:21:56 -0800

    [stm32f429i-discovery] Added fancyblink example.
    For some reason when setting the ram size to 256 in the linker file we
    are getting a memory access error. Needs more investigation.

---
+++ examples/stm32/f4/stm32f429i-discovery/fancyblink/fancyblink.c
@@ -0,0 +1,61 @@
+/*
+ * This file is part of the libopencm3 project.
+ *
+ * Copyright (C) 2009 Uwe Hermann <uwe@hermann-uwe.de>
+ * Copyright (C) 2011 Damjan Marion <damjan.marion@gmail.com>
+ * Copyright (C) 2011 Mark Panajotovic <marko@electrontube.org>
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+
+/* Set STM32 to 168 MHz. */
+static void clock_setup(void)
+{
+	rcc_clock_setup_hse_3v3(&hse_8mhz_3v3[CLOCK_3V3_168MHZ]);
+
+	/* Enable GPIOG clock. */
+	rcc_periph_clock_enable(RCC_GPIOG);
+}
+
+static void gpio_setup(void)
+{
+	/* Set GPIO13-14 (in GPIO port G) to 'output push-pull'. */
+	gpio_mode_setup(GPIOG, GPIO_MODE_OUTPUT,
+			GPIO_PUPD_NONE, GPIO13 | GPIO14);
+}
+
+int main(void)
+{
+	int i;
+
+	clock_setup();
+	gpio_setup();
+
+	/* Set two LEDs for wigwag effect when toggling. */
+	gpio_set(GPIOG, GPIO13);
+
+	/* Blink the LEDs (PG13 and PG14) on the board. */
+	while (1) {
+		/* Toggle LEDs. */
+		gpio_toggle(GPIOG, GPIO13 | GPIO14);
+		for (i = 0; i < 6000000; i++) { /* Wait a bit. */
+			__asm__("nop");
+		}
+	}
+
+	return 0;
+}
