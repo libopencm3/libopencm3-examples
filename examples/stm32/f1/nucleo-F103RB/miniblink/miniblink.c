24c50713ab08075d7881da794fe1f8cb3795c40c
LaurentBa <laurent@larueluberlu.net>
2015-02-16 17:37:32 +0100

    adding miniblink example for nucleo-F103RB

---
+++ examples/stm32/f1/nucleo-F103RB/miniblink/miniblink.c
@@ -0,0 +1,72 @@
+/*
+ * This file is part of the libopencm3 project.
+ *
+ * Copyright (C) 2009 Uwe Hermann <uwe@hermann-uwe.de>
+ * Copyright (C) 2015 Laurent Barattero <laurentba@larueluberlu.net>
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+
+static void gpio_setup(void)
+{
+	/* Enable GPIOA clock. */
+	/* Manually: */
+	//RCC_APB2ENR |= RCC_APB2ENR_IOPAEN;
+	/* Using API functions: */
+	rcc_periph_clock_enable(RCC_GPIOA);
+
+	/* Set GPIO5 (in GPIO port A) to 'output push-pull'. */
+	/* Manually: */
+	//GPIOA_CRL = (GPIO_CNF_OUTPUT_PUSHPULL << ((5 * 4) + 2));
+	//GPIOA_CRL |= (GPIO_MODE_OUTPUT_2_MHZ << (5 * 4));
+	/* Using API functions: */
+	gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_2_MHZ,
+		      GPIO_CNF_OUTPUT_PUSHPULL, GPIO5);
+}
+
+int main(void)
+{
+	int i;
+
+	gpio_setup();
+
+	/* Blink the LED (PA5) on the board. */
+	while (1) {
+		/* Manually: */
+		// GPIOA_BSRR = GPIO5;		/* LED off */
+		// for (i = 0; i < 800000; i++)	/* Wait a bit. */
+		// 	__asm__("nop");
+		// GPIOA_BRR = GPIO5;		/* LED on */
+		// for (i = 0; i < 800000; i++)	/* Wait a bit. */
+		// 	__asm__("nop");
+
+		/* Using API functions gpio_set()/gpio_clear(): */
+		// gpio_set(GPIOA, GPIO5);	/* LED off */
+		// for (i = 0; i < 800000; i++)	/* Wait a bit. */
+		// 	__asm__("nop");
+		// gpio_clear(GPIOA, GPIO5);	/* LED on */
+		// for (i = 0; i < 800000; i++)	/* Wait a bit. */
+		// 	__asm__("nop");
+
+		/* Using API function gpio_toggle(): */
+		gpio_toggle(GPIOA, GPIO5);	/* LED on/off */
+		for (i = 0; i < 80000; i++)	/* Wait a bit. */
+			__asm__("nop");
+	}
+
+	return 0;
+}
