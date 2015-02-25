c6af45ba8fe9cdaeb92b5e85740f13e1a5999b09
Piotr Esden-Tempski <piotr@esden.net>
2015-02-24 19:47:28 -0800

    [nucleo-f411re] Stylefix.

--- examples/stm32/f4/nucleo-f411re/blink/blink.c
+++ examples/stm32/f4/nucleo-f411re/blink/blink.c
@@ -26,14 +26,14 @@ static void gpio_setup(void)
 {
 	/* Enable GPIOA clock. */
 	/* Manually: */
-	// RCC_AHB1ENR |= RCC_AHB1ENR_IOPAEN;
+	/*RCC_AHB1ENR |= RCC_AHB1ENR_IOPAEN; */
 	/* Using API functions: */
 	rcc_periph_clock_enable(RCC_GPIOA);
 
 	/* Set GPIO5 (in GPIO port A) to 'output push-pull'. */
 	/* Manually: */
-	// GPIOA_CRH = (GPIO_CNF_OUTPUT_PUSHPULL << (((8 - 8) * 4) + 2));
-	// GPIOA_CRH |= (GPIO_MODE_OUTPUT_2_MHZ << ((8 - 8) * 4));
+	/*GPIOA_CRH = (GPIO_CNF_OUTPUT_PUSHPULL << (((8 - 8) * 4) + 2)); */
+	/*GPIOA_CRH |= (GPIO_MODE_OUTPUT_2_MHZ << ((8 - 8) * 4)); */
 	/* Using API functions: */
 	gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO5);
 }
@@ -47,20 +47,20 @@ int main(void)
 	/* Blink the LED (PC8) on the board. */
 	while (1) {
 		/* Manually: */
-		// GPIOA_BSRR = GPIO5;		/* LED off */
-		// for (i = 0; i < 1000000; i++)	/* Wait a bit. */
-		//	__asm__("nop");
-		// GPIOA_BRR = GPIO5;		/* LED on */
-		// for (i = 0; i < 1000000; i++)	/* Wait a bit. */
-		//	__asm__("nop");
+		/* GPIOA_BSRR = GPIO5; */		/* LED off */
+		/* for (i = 0; i < 1000000; i++) */	/* Wait a bit. */
+		/*	__asm__("nop"); */
+		/* GPIOA_BRR = GPIO5; */		/* LED on */
+		/* for (i = 0; i < 1000000; i++) */	/* Wait a bit. */
+		/*	__asm__("nop"); */
 
 		/* Using API functions gpio_set()/gpio_clear(): */
-		// gpio_set(GPIOA, GPIO5);	/* LED off */
-		// for (i = 0; i < 1000000; i++)	/* Wait a bit. */
-		//	__asm__("nop");
-		// gpio_clear(GPIOA, GPIO5);	/* LED on */
-		// for (i = 0; i < 1000000; i++)	/* Wait a bit. */
-		//	__asm__("nop");
+		/* gpio_set(GPIOA, GPIO5); */	/* LED off */
+		/* for (i = 0; i < 1000000; i++) */	/* Wait a bit. */
+		/*	__asm__("nop"); */
+		/* gpio_clear(GPIOA, GPIO5); */	/* LED on */
+		/* for (i = 0; i < 1000000; i++) */	/* Wait a bit. */
+		/*	__asm__("nop"); */
 
 		/* Using API function gpio_toggle(): */
 		gpio_toggle(GPIOA, GPIO5);	/* LED on/off */
