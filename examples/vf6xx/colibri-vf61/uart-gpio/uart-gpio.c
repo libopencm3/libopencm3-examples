a6c183d470a24291fb1edfd70201b46f9acf0f9d
Karl Palsson <karlp@remake.is>
2015-07-16 17:17:12 +0000

    vf6xx: merge gpio/uart examples
    The plain "uart" example was just a slightly less featured version.
    Compile tested only!

--- examples/vf6xx/colibri-vf61/uart-gpio/uart-gpio.c
+++ examples/vf6xx/colibri-vf61/uart-gpio/uart-gpio.c
@@ -23,6 +23,15 @@
 #include <libopencm3/cm3/common.h>
 #include <libopencm3/vf6xx/ccm.h>
 #include <libopencm3/vf6xx/uart.h>
+#include <libopencm3/vf6xx/gpio.h>
+#include <libopencm3/vf6xx/iomuxc.h>
+
+
+static const uint32_t gpio_out = IOMUXC_PAD(MUX_MODE_ALT0, SPEED_LOW, \
+			DSE_150OHM, PUS_PU_100KOHM, IOMUXC_OBE | IOMUXC_IBE);
+
+static const uint32_t gpio_in = IOMUXC_PAD(MUX_MODE_ALT0, SPEED_LOW, \
+			DSE_150OHM, PUS_PU_100KOHM, IOMUXC_IBE);
 
 int _write(int file, char *ptr, int len);
 
@@ -49,24 +58,41 @@ static void uart_init(void)
 
 int main(void)
 {
-	int counter = 0; 
-	float fcounter = 0.0;
-	double dcounter = 0.0;
+	int old_state = -1;
+	int new_state = 0;
+	int i = 0;
+	float f = 0.0f;
 
+
+	/* Init Clock Control and UART */
 	ccm_calculate_clocks();
 	uart_init();
 
+	/* Iris Carrier Board, X16, Pin 15 */
+	iomuxc_mux(PTC3, gpio_out);
+
+	/* Iris Carrier Board, X16, Pin 16 */
+	iomuxc_mux(PTC2, gpio_in);
+
+	gpio_set(PTC3);
+	printf("GPIO %d is %s\r\n", PTC3, gpio_get(PTC3) ? "high" : "low");
+
 	/*
-	 * Write Hello World an integer, float and double all over
-	 * again while incrementing the numbers.
+	 * Read input GPIO in a loop and print changes
+	 * A jumper between PTC3<=> PTC2 can be used to set PTC2 to high
+	 * print integer and floating point counters as well, just for fun.
 	 */
 	while (1) {
-		printf("Hello World! %i %f %f\r\n", counter, fcounter,
-		       dcounter);
-		counter++;
-		fcounter += 0.01;
-		dcounter += 0.01;
+		new_state = gpio_get(PTC2);
+		if (old_state != new_state) {
+			printf("loop: %d (step: %f) GPIO %d is %s\r\n",
+				i, f, PTC2, new_state ? "high" : "low");
+			old_state = new_state;
+		}
+		i++;
+		f += 0.01;
 	}
+
 
 	return 0;
 }
