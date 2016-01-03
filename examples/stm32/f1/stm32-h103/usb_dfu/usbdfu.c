a87b3e09ad0f0ae0f8d6e075b41961ebb3dd7e67
Zhiyuan Wan <rgwan@rocaloid.org>
2016-01-03 16:32:08 +0800

    stm32f103: fix all usb driver

--- examples/stm32/f1/stm32-h103/usb_dfu/usbdfu.c
+++ examples/stm32/f1/stm32-h103/usb_dfu/usbdfu.c
@@ -267,7 +267,7 @@ int main(void)
 		      GPIO_CNF_OUTPUT_PUSHPULL, GPIO11);
 	gpio_set(GPIOC, GPIO11);
 
-	usbd_dev = usbd_init(&stm32f107_usb_driver, &dev, &config, usb_strings, 4, usbd_control_buffer, sizeof(usbd_control_buffer));
+	usbd_dev = usbd_init(&st_usbfs_v1_usb_driver, &dev, &config, usb_strings, 4, usbd_control_buffer, sizeof(usbd_control_buffer));
 	usbd_register_set_config_callback(usbd_dev, usbdfu_set_config);
 
 	gpio_clear(GPIOC, GPIO11);
