e0127b20003a3f1a456813ddd4107cb9382588d4
Karl Palsson <karlp@tweak.net.au>
2016-10-04 20:50:17 +0000

    stm32f1: ubshid: add re-enumeration forcing.
    This makes the demo reliably work on "generic" "other" style f1 boards
    out of the box.

--- examples/stm32/f1/other/usb_hid/usbhid.c
+++ examples/stm32/f1/other/usb_hid/usbhid.c
@@ -279,6 +279,13 @@ int main(void)
 	rcc_clock_setup_in_hsi_out_48mhz();
 
 	rcc_periph_clock_enable(RCC_GPIOA);
+	/* Hack to force renumeration */
+	gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_2_MHZ,
+		GPIO_CNF_OUTPUT_PUSHPULL, GPIO12);
+	gpio_clear(GPIOA, GPIO12);
+	for (unsigned i = 0; i < 800000; i++) {
+		__asm__("nop");
+	}
 
 	usbd_dev = usbd_init(&st_usbfs_v1_usb_driver, &dev_descr, &config, usb_strings, 3, usbd_control_buffer, sizeof(usbd_control_buffer));
 	usbd_register_set_config_callback(usbd_dev, hid_set_config);
