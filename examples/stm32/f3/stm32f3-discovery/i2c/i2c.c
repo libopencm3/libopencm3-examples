4d74cd31982a744bd5dcedd40de1f730e855d317
C47D <carlos.santiago.diaz@gmail.com>
2018-01-19 01:26:31 -0600

    Fixes #167

--- examples/stm32/f3/stm32f3-discovery/i2c/i2c.c
+++ examples/stm32/f3/stm32f3-discovery/i2c/i2c.c
@@ -58,7 +58,7 @@ static void i2c_setup(void)
 	i2c_peripheral_disable(I2C1);
 	//configure ANFOFF DNF[3:0] in CR1
 	i2c_enable_analog_filter(I2C1);
-	i2c_set_digital_filter(I2C1, I2C_CR1_DNF_DISABLED);
+	i2c_set_digital_filter(I2C1, 0);
 	/* HSI is at 8Mhz */
 	i2c_set_speed(I2C1, i2c_speed_sm_100k, 8);
 	//configure No-Stretch CR1 (only relevant in slave mode)
