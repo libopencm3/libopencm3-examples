4523323028bca46f2380f7a05c78ef2f16b2a168
Vitali Samurov <vitali.samurov@live.com>
2018-06-01 07:58:11 +0300

    Typo fixed (clean)

--- examples/rules.mk
+++ examples/rules.mk
@@ -210,7 +210,7 @@ print-%:
 
 clean:
 	@#printf "  CLEAN\n"
-	$(Q)$(RM) *.o *.d *.elf *.bin *.hex *.srec *.list *.map generated.* ${OBJS} ${OBJS:%.o:%.d}
+	$(Q)$(RM) *.o *.d *.elf *.bin *.hex *.srec *.list *.map generated.* ${OBJS} ${OBJS:%.o=%.d}
 
 stylecheck: $(STYLECHECKFILES:=.stylecheck)
 styleclean: $(STYLECHECKFILES:=.styleclean)
