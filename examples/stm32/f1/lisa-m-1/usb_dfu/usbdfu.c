408e20dddc66f112621d71c0ad21fc65fe2b081d
Daniel Gröber <dxld@darkboxed.org>
2020-04-28 14:03:18 +0200

    Add docs for DfuSe descriptor string

--- examples/stm32/f1/lisa-m-1/usb_dfu/usbdfu.c
+++ examples/stm32/f1/lisa-m-1/usb_dfu/usbdfu.c
@@ -79,8 +79,7 @@ const struct usb_interface_descriptor iface = {
 	.bInterfaceSubClass = 1,
 	.bInterfaceProtocol = 2,
 
-	/* The ST Microelectronics DfuSe application needs this string.
-	 * The format isn't documented... */
+	/* The ST Microelectronics DfuSe application needs this string. */
 	.iInterface = 4,
 
 	.extra = &dfu_function,
@@ -109,8 +108,30 @@ static const char *usb_strings[] = {
 	"Black Sphere Technologies",
 	"DFU Demo",
 	"DEMO",
-	/* This string is used by ST Microelectronics' DfuSe utility. */
+        // DfuSe descriptor string, used by ST DfuSe utility and also
+        // supported by dfu-util, see UM0424 section 10.3.2:
+        //
+        //                 |----------[zone]----------|
+        //                            [segment]
+        //                             \-----/
 	"@Internal Flash   /0x08000000/8*001Ka,56*001Kg",
+        // start address of [zone]-^   | |--/| \-------/
+        //                             | |   | [segment]
+        //                             | |   |
+        //                             | |    ` [flags], "a"=readable
+        // [flags]:                    |  ` 1K bytes per erase-block
+        //   (a) readable              `8 erase-blocks
+        //   (b) earaseable
+        //   (c) readable and earaseable
+        //   (e) readable and writable
+        //   (f) earaseable and writable
+        //   (g) readable, earaseable and writable
+        //
+        // [zone]: a number of segments starting at given address, allowed
+        //   to be non-contigous
+        //
+        // [segment]: a number of contigous erase blocks, or "sectors" in
+        //   ST docs
 };
 
 static uint8_t usbdfu_getstatus(uint32_t *bwPollTimeout)
