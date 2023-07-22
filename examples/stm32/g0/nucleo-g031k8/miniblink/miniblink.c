8405488be937ecf0e28df7b12e7dbc744ca4d5b0
Ryan Walker <ryan.cjw@gmail.com>
2023-07-22 12:41:01 -0700

    Added some examples for the g0

---
+++ examples/stm32/g0/nucleo-g031k8/miniblink/miniblink.c
@@ -0,0 +1,75 @@
+/*
+ * This file is part of the libopencm3 project.
+ *
+ * Copyright (C) 2009 Uwe Hermann <uwe@hermann-uwe.de>
+ * Copyright (C) 2011 Stephen Caudle <scaudle@doceme.com>
+ * Copyright (C) 2012 Karl Palsson <karlp@tweak.net.au>
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/i2c.h>
+
+#define PORT_LED GPIOC
+#define PIN_LED GPIO6
+
+static void gpio_setup(void)
+{
+	/* Enable GPIOC clock. */
+	/* Manually: */
+	//RCC_AHBENR |= RCC_AHBENR_GPIOCEN;
+	/* Using API functions: */
+	rcc_periph_clock_enable(RCC_GPIOC);
+
+
+	/* Set GPIO8 (in GPIO port C) to 'output push-pull'. */
+	/* Using API functions: */
+	gpio_mode_setup(PORT_LED, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, PIN_LED);
+}
+
+int main(void)
+{
+	int i;
+
+	gpio_setup();
+
+	/* Blink the LED (PC8) on the board. */
+	while (1) {
+		/* Manually: */
+		// GPIOC_BSRR = PIN_LED;		/* LED off */
+		// for (i = 0; i < 1000000; i++)	/* Wait a bit. */
+		//	__asm__("nop");
+		// GPIOC_BRR = PIN_LED;			/* LED on */
+		// for (i = 0; i < 1000000; i++)	/* Wait a bit. */
+		//	__asm__("nop");
+
+		/* Using API functions gpio_set()/gpio_clear(): */
+		// gpio_set(PORT_LED, PIN_LED);		/* LED off */
+		// for (i = 0; i < 1000000; i++)	/* Wait a bit. */
+		//	__asm__("nop");
+		// gpio_clear(PORT_LED, PIN_LED);	/* LED on */
+		// for (i = 0; i < 1000000; i++)	/* Wait a bit. */
+		//	__asm__("nop");
+
+		/* Using API function gpio_toggle(): */
+		gpio_toggle(PORT_LED, PIN_LED);	/* LED on/off */
+		for (i = 0; i < 1000000; i++) {	/* Wait a bit. */
+			__asm__("nop");
+		}
+	}
+
+	return 0;
+}
